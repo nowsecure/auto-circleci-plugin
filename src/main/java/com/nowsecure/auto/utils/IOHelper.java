17c10e394bfee46bb72b595eef5a1b6a9d4f942f
Shahzad Bhatti <sbhatti@nowsecure.com>
2019-06-27 11:19:07 -0700

    updated http error

--- src/main/java/com/nowsecure/auto/utils/IOHelper.java
+++ src/main/java/com/nowsecure/auto/utils/IOHelper.java
@@ -176,6 +176,12 @@ public class IOHelper implements IOHelperI {
         con.setRequestProperty(CONTENT_LENGTH, String.valueOf(binary.length));
         con.setRequestProperty(CONTENT_DIGEST, toDigest(binary, "SHA-256"));
         con.setDoOutput(true);
+        con.connect();
+        // Check http code first
+        int code = con.getResponseCode();
+        if (code < 200 || code >= 300) {
+            throw new IOException("Failed to connect to " + uri + " to upload " + file + " due to HTTP status " + code + ", message " + con.getResponseMessage() + " -- " + con.getContent());
+        }
         OutputStream out = con.getOutputStream();
         out.write(binary);
         out.flush();
